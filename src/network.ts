990dc7e0f7a1d3d05e7a7239fce8da4af63b17d9
showtime254 <sindi.guza@tracktik.com>
2024-07-15 13:14:49 +0200

    change bytes length

--- src/network.ts
+++ src/network.ts
@@ -398,7 +398,7 @@ export class DynamicUnicastNetwork extends Network {
 }
 
 function encrypt(data: Buffer, key: string) {
-  const cipher = crypto.createCipheriv('aes256', Buffer.from(key, 'hex'), crypto.randomBytes(32))
+  const cipher = crypto.createCipheriv('aes256', Buffer.from(key, 'hex'), crypto.randomBytes(16))
   const buf = []
   buf.push(cipher.update(data))
   buf.push(cipher.final())
